f9a0f8b57799f66f5af892245c8937d46c884d6d
Quincy Chisaina <113616008+Chisaina69@users.noreply.github.com>
2023-09-07 12:10:46 +0300

    Changed some seeded data

--- app/seed.py
+++ app/seed.py
@@ -1,43 +1,49 @@
-from datetime import datetime
-from models import Base, engine, session, Restaurant, Inspection, InspectionResult
-from sqlalchemy.exc import OperationalError
+from datetime import date
+from models import engine, session, Restaurant, Inspection, InspectionResult
 from faker import Faker
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
 
 fake = Faker()
 
-def ensure_database_exists():
-    try:
-        # Try to query the restaurant table
-        session.query(Restaurant).first()
-    except OperationalError:
-        # If we get an OperationalError, the table probably doesn't exist
-        print("Creating database tables...")
-        Base.metadata.create_all(engine)
-        print("Tables created!")
+set_dates = [date(2023, 9, i) for i in range(1, 21)]
 
-def seed_database():
-    # Number of restaurants to seed
-    num_restaurants = 100
-    
-    for _ in range(num_restaurants):
+
+if __name__ == "__main__":
+
+    engine = create_engine('sqlite:///diner_tracker.db')
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    session.query(Restaurant).delete()
+    session.query(Inspection).delete()
+    session.query(InspectionResult).delete()
+    session.commit()
+
+    print("seeding.....")
+
+    restaurant = []
+    for i in range(20):
         restaurant_name = fake.company()
-        health_rating = fake.random_element(elements=("A", "B", "C", "D", "F"))
-        restaurant = Restaurant(name=restaurant_name, health_rating=health_rating)
+        health_rating = fake.random_element(
+            elements=("A", "B", "C", "D", "F"))
+        restaurant = Restaurant(
+            name=restaurant_name, health_rating=health_rating)
         session.add(restaurant)
 
         # Create inspections for those restaurants
-        inspection_date = fake.date_this_year(before_today=True, after_today=False)
+        inspection_date = set_dates[i]
         inspector_name = fake.first_name()
-        inspection = Inspection(inspector=inspector_name, date=datetime.strptime(inspection_date, "%Y-%m-%d").date(), restaurant=restaurant)
+        inspection = Inspection(inspector=inspector_name, date=(
+            inspection_date), restaurant=restaurant)
         session.add(inspection)
 
         # Create inspection results for those inspections
-        result_text = fake.random_element(elements=("Clean", "Not Clean", "Needs Improvement", "Excellent"))
-        result = InspectionResult(results=result_text, inspection=inspection)
+        result_text = fake.random_element(
+            elements=("Clean", "Not Clean", "Needs Improvement", "Excellent"))
+        result = InspectionResult(
+            results=result_text, inspection=inspection)
         session.add(result)
-    
-    session.commit()
 
-if __name__ == "__main__":
-    ensure_database_exists()
-    seed_database()
+    session.commit()
